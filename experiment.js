98be27eaaead8bd91dc1c7ca648beea9c08ea04f
Raghav B. Venkataramaiyer <bvraghav@gmail.com>
2024-04-08 12:32:18 +0530

    Update clock draw routine.

--- experiment.js
+++ experiment.js
@@ -1,7 +1,7 @@
 class Experiment {
   // Group Details
   static rollNos = '10983437,10983743'
-  static names = 'The Tutors(Akhtar Banga, Phul Tekchand)'
+  static names = 'The Tutors(Akhtar Banga, Raghav)'
 
   canvasSel = '#myCanvas'
 
