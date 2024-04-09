ef31ee16d5d991c8b84f17a638cc0a614f581950
Raghav B. Venkataramaiyer <bvraghav@gmail.com>
2024-04-09 12:43:30 +0530

    Update for efforts in lab.

--- experiment.js
+++ experiment.js
@@ -17,14 +17,16 @@ class Experiment {
     // Clock
     // --------------------------------------------------
     const clock = new Clock(this.canvasSel)
-    // const ms = document.timeline.currentTime
-    // clock.draw(ms)
-    // clock.draw(ms+25000)
+    // // const ms = document.timeline.currentTime
+    // // clock.draw(ms)
+    // // clock.draw(ms+25000)
     const clockRafFn = (ts) => {
       clock.draw(ts)
       window.requestAnimationFrame(clockRafFn)
     }
     const clockRaf = window.requestAnimationFrame(clockRafFn)
+
+    
     
   }
 
