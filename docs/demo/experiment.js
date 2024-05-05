2dc3fabae707edc2676456aae01727df9118a02c
Alok Shree Koirala <akoirala_be21@thapar.edu>
2024-05-05 23:40:41 +0530

    fit according to template

---
+++ docs/demo/experiment.js
@@ -0,0 +1,40 @@
+class Experiment {
+  // Group Details
+  static rollNos = '102103157,102103178'
+  static names = 'The Pheonix(Alok Shree Koirala, Shree Krihna Singla)'
+
+  canvasSel = '#myCanvas' 
+
+  run(){
+
+  }
+
+  runSteppers() {
+
+    // Steppers
+    // --------------------------------------------------
+    const stepperOneRaf
+      = window.requestAnimationFrame(stepperOne)
+
+    const stepperTwoRaf
+      = window.requestAnimationFrame(stepperTwo)
+
+    const stepperThree = new StepperThree(
+      '#valueFromStepperThree', 15
+    )
+    const stepperThreeFn = (ts) => {
+      if (!stepperThree.isActive) stepperThree.start()
+      stepperThree.step(ts)
+      window.requestAnimationFrame(stepperThreeFn)
+    }
+    const stepperThreeRaf
+      = window.requestAnimationFrame(stepperThreeFn)
+    // --------------------------------------------------
+    
+  }
+
+  hideSteppers() {
+    document.querySelector('#side-note')
+      .classList.add('hidden')
+  }
+}
